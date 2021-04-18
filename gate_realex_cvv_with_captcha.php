02dfe5450964f8bf1441fc89e664e14fb1297f7e
Jason Te <39984351+pnrjason@users.noreply.github.com>
2021-04-18 10:10:16 +0800

    realex cvv with automated captcha solver (invisible recaptcha v2)

--- gate_realex_cvv_with_captcha.php
+++ gate_realex_cvv_with_captcha.php
@@ -50,7 +50,7 @@
             'accept: */*',
             'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36'
         )
-    ));
+    )); 
     curl_exec($ch);
     curl_close($ch);
 
