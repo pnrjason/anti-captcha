ce899e13bd0806c09431dca21d80dfd9b78a33de
Jason Te <39984351+pnrjason@users.noreply.github.com>
2021-04-18 10:09:20 +0800

    wc + realex ccn2

--- gate_realex3_ccn.php
+++ gate_realex3_ccn.php
@@ -106,7 +106,7 @@
         CURLOPT_HTTPHEADER => array(
             'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
             'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36'
-        )
+        ) 
     ));
     foreach(explode("<input type=\"hidden\"", GetStr(curl_exec($ch), '<form action="https://pay.realexpayments.com/pay" method="post">', "</form>")) as $values){
         $form[GetStr($values, 'name="','"')] = GetStr($values, 'value="','"');
